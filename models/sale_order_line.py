b6d6394af3be372af7e077f7c51a672763c48ccc
BrainessCompany <brainesscompany@gmail.com>
2024-12-23 12:45:29 -0300

    inclui modelo sale_order_line

---
+++ models/sale_order_line.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    brand_id = fields.Many2one(
+        comodel_name='product.brand',
+        string='Brand',
+        related='product_id.product_tmpl_id.brand_id',
+        store=True,
+    )
+
+
