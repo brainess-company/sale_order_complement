c56b760d438a28bb6ee2cbcf80706e4d4ba4eabf
BrainessCompany <brainesscompany@gmail.com>
2024-12-22 21:20:44 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Custom NCM and Brand Fields',
+    'version': '14.0.1.0.0',
+    'summary': 'Add NCM and Brand fields to sale order lines and customer views.',
+    'author': 'Your Name',
+    'license': 'LGPL-3',
+    'depends': ['sale', 'l10n_br_fiscal', 'product_brand'],  # Adicione os módulos necessários
+    'data': [
+    'views/sale_order_line_views.xml',  # Já configurado
+    'views/portal_sale_order_template.xml',  # Portal
+    'views/report_sale_order.xml',  # Relatório PDF
+],
+    'installable': True,
+    'application': False,
+}
