161a3d1784832242cf7c86a5cb6adbd59a482d99
BrainessCompany <brainesscompany@gmail.com>
2025-03-04 19:14:35 -0300

    branch alterada  para a 16

--- __manifest__.py
+++ __manifest__.py
@@ -1,8 +1,8 @@
 {
     'name': 'Custom NCM and Brand Fields',
-    'version': '14.0.1.0.0',
+    'version': '16.0.1.0.0',
     'summary': 'Add NCM and Brand fields to sale order lines and customer views.',
-    'author': 'Your Name',
+    'author': 'Brainess Company',
     'license': 'LGPL-3',
     'depends': ['sale',
                 'l10n_br_sale',
@@ -10,8 +10,6 @@
                 'product',
                 'product_brand_inventory'],  # Adicione os módulos necessários ,
     'data': [
-        #'security/security.xml',
-        #'security/ir.model.access.csv',
         'views/sale_order_line_views.xml',  # Já configurado
         # 'views/portal_sale_order_template.xml',  # Portal
         'views/report_sale_order.xml',  # Relatório PDF
